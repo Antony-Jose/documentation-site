69e5fdb7834f09b9352e761d1bd2271ba4a22c94
itsmeneil23 <128038372+itsmeneil23@users.noreply.github.com>
2023-11-12 11:26:57 +0530

    changed
    added the

--- Documentation/faculty/views.py
+++ Documentation/faculty/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def faculty(request):
-    return HttpResponse('this is faculty page')
+    return HttpResponse('this is the faculty page')
