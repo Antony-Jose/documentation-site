be918a31a21adad127962b91fb1bc7a377f48895
Antony-Jose <67753125+Antony-Jose@users.noreply.github.com>
2023-11-21 23:49:00 +0530

    hod & faculty login

--- Documentation/HOD/views.py
+++ Documentation/HOD/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def HODLogin(request):
-    return render(request,'HOD/login.html',{})
-
 @login_required(login_url='/HODLogin')
 def HOD(request):
-    return HttpResponse('this is hod page')
+    return render(request, 'HOD/home.html')
+
+def HODLogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # User is valid, log them in
+            login(request, user)
+            return render(request, 'HOD/home.html')  # Replace 'home' with the URL you want to redirect to after successful login
+        else:
+            # Invalid login credentials
+            return render(request, 'HOD/login.html', {'error_message': 'Invalid username or password'})
+ 
+    return render(request, 'HOD/login.html')
--- Documentation/faculty/views.py
+++ Documentation/faculty/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render
+
 # Create your views here.
-def facultyLogin(request):
-    return render(request,'faculty/login.html',{})
+
 
 @login_required(login_url='/facultyLogin')
 def faculty(request):
-    return HttpResponse('this is the faculty page')
+    return render(request, 'faculty/home.html')
+
+
+
+def facultyLogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # User is valid, log them in
+            login(request, user)
+            return render(request, 'faculty/home.html')  # Replace 'home' with the URL you want to redirect to after successful login
+        else:
+            # Invalid login credentials
+            return render(request, 'faculty/login.html', {'error_message': 'Invalid username or password'})
+
+    return render(request, 'faculty/login.html')
